687136f3196ec988aa8f20a773a338cea7ad6e58
lehhh <leandro.tacara@hotmail.com>
2018-08-26 15:39:16 -0300

    test domain categoria

--- src/main/java/br/com/ltacara/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/ltacara/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,29 @@
 package br.com.ltacara.cursomc.resources;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ltacara.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public ArrayList<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		ArrayList<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		
+		return lista;
 	}
 
-	
 }
